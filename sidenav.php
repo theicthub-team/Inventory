cd7e60cb9fbdffc6dd900e1be72209e9c6aede9c
Maruf Hasan <35674879+Marufhasan74@users.noreply.github.com>
2019-07-08 04:27:45 +0600

    Add files via upload
    8july19

---
+++ sidenav.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>
+    Inventory Hub - Dashboard
+  </title>
+  <!-- Favicon -->
+  <link href="./assets/img/brand/favicon.png" rel="icon" type="image/png">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <!-- Icons -->
+  <link href="./assets/js/plugins/nucleo/css/nucleo.css" rel="stylesheet" />
+  <link href="./assets/js/plugins/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link href="./assets/css/argon-dashboard.css?v=1.1.0" rel="stylesheet" />
+  <!---Custom CSS here-->
+  <link rel="stylesheet" type="text/css" href="customStyle.css">
+</head>
+<body class="">
+  <div class="container">
+  <nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light bg-dark" id="sidenav-main">
+     <div class="container-fluid">
+       <!-- Toggler -->
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+       </button>
+       <!-- Brand -->
+       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
+         <div class="sidebar-brand-icon rotate-n-15">
+           <img src="./assets/img/logo.png">
+         </div>
+         <div class="sidebar-brand-text mx-3 text-white font-weight-bold text-uppercase">Inventory Hub</div>
+       </a>
+          <!-- Collapse -->
+       <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+         <!-- Collapse header -->
+         <div class="navbar-collapse-header d-md-none">
+           <div class="row">
+             <div class="col-6 collapse-brand">
+               <a href="./index.html">
+                 <img src="./assets/img/brand/blue.png">
+               </a>
+             </div>
+             <div class="col-6 collapse-close">
+               <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
+                 <span></span>
+                 <span></span>
+               </button>
+             </div>
+           </div>
+         </div>
+         <!-- Navigation -->
+         <ul class="navbar-nav">
+           <li class="nav-item  class=" active" ">
+           <a class=" nav-link active text-white " href=""> <i class="ni ni-tv-2 text-primary"></i> Dashboard
+             </a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link text-white " href="">
+               <i class="ni ni-single-02 text-yellow"></i> Contact
+             </a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link text-white " href="">
+               <i class="ni ni-planet text-blue"></i> Inventory
+             </a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link text-white " href="">
+               <i class="ni ni-bullet-list-67 text-red"></i> Invoices
+             </a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link text-white " href="">
+               <i class="ni ni-pin-3 text-orange"></i> calendar
+             </a>
+           </li>
+
+           <li class="nav-item">
+             <a class="nav-link text-white " href="">
+               <i class="ni ni-circle-08 text-pink"></i> Help center
+             </a>
+           </li>
+         </ul>
+         <!-- Divider -->
+         <hr class="my-3">
+         <!-- Heading -->
+         <h6 class="navbar-heading text-muted">Add-ons</h6>
+         <!-- Navigation -->
+         <ul class="navbar-nav mb-md-3">
+           <li class="nav-item">
+             <a class="nav-link text-white" href="">
+               <i class="ni ni-spaceship"></i> Chat Room
+             </a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link text-white " href="">
+               <i class="ni ni-ui-04"></i> Settings
+             </a>
+           </li>
+         </ul>
+       </div>
+     </div>
+   </nav>
+ </div>
+
+  <script src="./assets/js/plugins/jquery/dist/jquery.min.js"></script>
+    <script src="./assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!--   Optional JS   -->
+    <script src="./assets/js/plugins/chart.js/dist/Chart.min.js"></script>
+    <script src="./assets/js/plugins/chart.js/dist/Chart.extension.js"></script>
+    <!--   Argon JS   -->
+    <script src="./assets/js/argon-dashboard.min.js?v=1.1.0"></script>
+    <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
+  </body>
+
+</html>
