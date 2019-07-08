a8162b9001a84577d7f879f0120247310c3e1d1a
Abdullah Al Rifat <abdullahalrifat95@gmail.com>
2019-07-08 18:46:46 +0600

    final layout

---
+++ view/footer.php
@@ -0,0 +1,11 @@
+<script src="./assets/js/plugins/jquery/dist/jquery.min.js"></script>
+<script src="./assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<!--   Optional JS   -->
+<script src="./assets/js/plugins/chart.js/dist/Chart.min.js"></script>
+<script src="./assets/js/plugins/chart.js/dist/Chart.extension.js"></script>
+<!--   Argon JS   -->
+<script src="./assets/js/argon-dashboard.min.js?v=1.1.0"></script>
+<script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
+</body>
+
+</html>
