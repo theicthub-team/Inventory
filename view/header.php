21248301bc8962234afd21d71483f020e9f9fe86
Marufhasan74 <marufiub9@gmail.com>
2019-07-12 23:47:16 +0600

    12July(All Table Done)(version.1)

--- view/header.php
+++ view/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>
-        Inventory Hub - Dashboard
+        Inventory Hub
     </title>
     <!-- Favicon -->
     <link href="assets/img/brand/favicon.png" rel="icon" type="image/png">
