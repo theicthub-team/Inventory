ee5dd8f7e03d61aabcac24e689b1db1db2c58488
Mujurina Azam <munjurinaa@gmail.com>
2019-07-20 19:28:06 +0600

    20 july 2019

---
+++ view/layout_printing_bill.php
@@ -0,0 +1,188 @@
+<?php include 'header.php'; ?>
+<body class="">
+<?php include 'sidenav.php'; ?>
+<div class="main-content">
+    <?php include 'navbar.php'; ?>
+
+    <!-- main content here -->
+
+    <!--    <div class="header pt-5">-->
+    <!--        <div class="container-fluid">-->
+    <!--            <div class="header-body">-->
+    <!--                <!-- Card stats -->
+    <!---->
+    <!--            </div>-->
+    <!--        </div>-->
+    <!--    </div>-->
+
+    <div class="container-fluid mt--7 pb-8 pt-5 pt-md-8">
+        <!-- main content here -->
+
+        <div class="row box_container">
+            <div class="col">
+                <div class="card shadow">
+                    <div class="card-header border-0">
+                        <h1 class="mb-0 font-weight-bold text-uppercase text-left paid">Paid</h1>
+                        <div class="address">
+                            <h1 class="mb-0 font-weight-bold text-uppercase text-right">BILL</h1>
+                            <h2 class="mb-0 font-weight-bold text-uppercase text-left">Inventory Hub</h2>
+
+                        </div>
+
+                        <h6 class="mb-0 font-weight-bold text-uppercase text-right">bill#001m </h6>
+
+                        <br>
+                        <br>
+                        <h5 class="balance text-right">Balance Due</h5>
+                        <h3 class="balacetk text-right">BDT-0.00</h3>
+                        <br>
+
+
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    Bill From:
+                    <a href="#">Mr.XYZ</a>
+                    <div class="row">
+                        <div class="col-lg-10 text-right">
+                            <div class="form-group">
+                                Order Number:
+                            </div>
+                        </div>
+                        <div class="col-lg-2 text-right">
+                            <div class="form-group">
+                                PO:#0001234
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div class="row">
+                        <div class="col-lg-10 text-right">
+                            <div class="form-group">
+                                Bill Date:
+                            </div>
+                        </div>
+                        <div class="col-lg-2 text-right">
+                            <div class="form-group">
+                                29 July 2019
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-10 text-right">
+                            <div class="form-group">
+                                Due Date:
+                            </div>
+                        </div>
+                        <div class="col-lg-2 text-right">
+                            <div class="form-group">
+                                30 July 2019
+                            </div>
+                        </div>
+                    </div>
+                    <div class="">
+                        <!-- Projects table -->
+                        <table class="table align-items-center table-flush">
+                            <thead class="thead-light">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Item & Description</th>
+                                <th scope="col">Quantity </th>
+                                <th scope="col">Rate</th>
+                                <th scope="col">Amount</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">
+                                    1
+                                </th>
+                                <td>
+                                    samsung J5
+                                </td>
+                                <td>
+                                    2
+                                </td>
+                                <td>
+                                    45
+                                </td>
+                                <td>
+                                    20,000
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <hr>
+
+                        <div class="row">
+                            <div class="col-lg-9 text-right">
+                                <div class="form-group">
+                                    Subtotal
+                                </div>
+                            </div>
+                            <div class="col-lg-2 text-right">
+                                <div class="form-group">
+                                    20,000
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-9 text-right">
+                                <div class="form-group">
+                                    Total
+                                </div>
+                            </div>
+                            <div class="col-lg-2 text-right">
+                                <div class="form-group">
+                                    BDT- 20,000
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-9 text-right">
+                                <div class="form-group">
+                                    Payment Mode
+                                </div>
+                            </div>
+                            <div class="col-lg-2 text-right">
+                                <div class="form-group payment">
+                                    (-)20,000
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+
+                    </div>
+
+
+
+
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <!-- end of main content-->
+
+
+
+
+        <!-- end of main content-->
+
+
+
+        <?php include 'footer.php'; ?>
+
+
+
