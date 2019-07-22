1ce24e555b5cc0c23621080c53cd9a8d34552b02
Marufhasan74 <marufiub9@gmail.com>
2019-07-22 17:03:10 +0600

    22July19

---
+++ view/LayoutAdjustContact.php
@@ -0,0 +1,245 @@
+<?php include 'header.php'; ?>
+<body class="">
+<?php include 'sidenav.php'; ?>
+<div class="main-content">
+    <?php include 'navbar.php'; ?>
+
+
+    <div class="header pt-5">
+        <div class="container-fluid">
+            <div class="header-body">
+                <!-- Card stats -->
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid mt--7 pb-8 pt-5 pt-md-8">
+
+
+        <div class="row box_container">
+            <div class="col">
+                <!-- main content here -->
+                <div class="">
+                    <div class="row">
+                        <div class="col-md-4 com-right">
+                            <div class="col-xl-20 mb-5 mb-xl-0">
+                                <div class="card shadow">
+                                    <div class="card-header border-0">
+                                        <div class="row align-items-center">
+                                            <div class="col">
+                                                <h3 class="mb-2">Contact List</h3>
+                                            </div>
+
+                                            <div class="dropdown">
+                                                <a class="btn btn-sm btn-icon-only text-light border-primary" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-ellipsis-v"></i>
+                                                </a>
+                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                                    <div class=" dropdown-header noti-title">
+                                                        <h6 class="text-overflow m-0 text-uppercase text-muted">Sort By:</h6>
+                                                    </div>
+                                                    <a class="dropdown-item" href="#">Created Time</a>
+                                                    <a class="dropdown-item" href="#">Last Modified Time</a>
+                                                    <a class="dropdown-item" href="#">Date</a>
+                                                    <a class="dropdown-item" href="#">Expected Shipment Date </a>
+                                                    <a class="dropdown-item" href="#">Import Sales Order</a>
+                                                    <a class="dropdown-item" href="#">Export Sales Order</a>
+                                                    <div class="dropdown-divider"></div>
+                                                    <a class="dropdown-item text-warning" href="#">Delete</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="table-responsive">
+                                        <!-- Projects table -->
+                                        <table class="table align-items-center table-flush">
+                                            <thead class="thead-light">
+                                            <tr>
+                                                <th scope="col">Check</th>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Company Name</th>
+
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+                                                <td>
+                                                    <div class="custom-control custom-checkbox mb-3">
+                                                        <input type="checkbox" class="custom-control-input" id="customCheck1" >
+                                                        <label class="custom-control-label" for="customCheck1"></label>
+                                                    </div>
+                                                </td>
+                                                <th scope="row">
+                                                    <a href="#">Sadik Ahammed</a>
+                                                </th>
+                                                <td>
+                                                    The ICT Hub
+                                                </td>
+
+
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <div class="custom-control custom-checkbox mb-3">
+                                                        <input type="checkbox" class="custom-control-input" id="customCheck2">
+                                                        <label class="custom-control-label" for="customCheck2"></label>
+                                                    </div>
+                                                <th scope="row">
+                                                    <a href="#">Muzahidul Islam</a>
+                                                </th>
+                                                <td>
+                                                    Samsung
+                                                </td>
+
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <div class="custom-control custom-checkbox mb-3">
+                                                        <input type="checkbox" class="custom-control-input" id="customCheck3">
+                                                        <label class="custom-control-label" for="customCheck3"></label>
+                                                    </div>
+                                                </td>
+                                                <th scope="row">
+                                                    <a href="#">Munjurina Azam Ema</a>
+                                                </th>
+                                                <td>
+                                                    City Bank
+                                                </td>
+
+                                            </tr>
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-8 com-left">
+                            <div class="row box_container">
+                                <div class="col">
+                                    <!-- main content here -->
+                                    <div class="card shadow">
+                                        <div class="card-header border-0">
+                                            <h3 class="mb-2 font-weight-bold text-uppercase">New Contact</h3>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="card-body">
+
+                                        <form>
+                                            <!--                        Contact Type-->
+                                            <h6 class="heading-small text-muted mb-4 ">Contact Type</h6>
+
+                                            <hr class="my-4" />
+                                            <h6 class="heading-small text-muted mb-4">User information</h6>
+                                            <div class="pl-lg-4">
+                                                <div class="row">
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="input-username">Full Name</label>
+                                                            <input type="text" id="input-username" class="form-control form-control-alternative" placeholder="First name + Last Name" value="First name + Last Name">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="input-email">Email address</label>
+                                                            <input type="email" id="input-email" class="form-control form-control-alternative" placeholder="user@example.com">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="input-first-name">Company Name</label>
+                                                            <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="Company Name" value="Company Name">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="input-last-name">Contact Display Name</label>
+                                                            <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Contact Display Name" value="Contact Display Name">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <hr class="my-4" />
+                                            <!-- Address -->
+                                            <h6 class="heading-small text-muted mb-4">Contact information</h6>
+                                            <div class="pl-lg-4">
+                                                <div class="row">
+                                                    <div class="col-md-12">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="input-address">Address</label>
+                                                            <input id="input-address" class="form-control form-control-alternative" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" type="text">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-lg-4">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="input-city">City</label>
+                                                            <input type="text" id="input-city" class="form-control form-control-alternative" placeholder="City" value="New York">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-4">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="input-country">Country</label>
+                                                            <input type="text" id="input-country" class="form-control form-control-alternative" placeholder="Country" value="United States">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-4">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="input-country">Postal code</label>
+                                                            <input type="number" id="input-postal-code" class="form-control form-control-alternative" placeholder="Postal code">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-md-12">
+                                                        <div class="form-group">
+                                                            <label class="form-control-label" for="input-address">Contact Phone</label>
+                                                            <input id="input-address" class="form-control form-control-alternative" placeholder="Home Address" value="+8801XXXXXXXXX" type="text">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <hr class="my-4" />
+                                            <!-- Save -->
+                                            <div class="col text-left">
+                                                <a href="LayoutAllContact.php" class="btn btn-md btn-primary">Add Contact</a>
+                                            </div>
+
+
+                                        </form>
+                                    </div>
+
+                                    <!-- end of main content-->
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div
+
+
+                    <!-- end of main content-->
+
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+
+    <?php include 'footer.php'; ?>
+
+
+
+
+
+
